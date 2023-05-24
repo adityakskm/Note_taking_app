2787b204fb6ff37e3876e0b6d70e9b411c2b5a89
adityakskm <93266818+adityakskm@users.noreply.github.com>
2023-05-24 21:03:01 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Sample Note Title</h1>
+      <p>
+        Enter sample text here... The simplest way to keep notes, Save your
+        thoughts, wherever you are
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
